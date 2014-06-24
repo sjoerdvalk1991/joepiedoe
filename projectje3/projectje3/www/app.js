8cd7d095c9b68ee9ae76cc333d6661d683ea01e1
Floyd van Boksel <floyd.van.boksel@gmail.com>
2014-06-24 10:57:43 +0200

    General suggestions + app.js cleanup
    Added some general suggestions plus cleaned up the app.js plus added tabs and such to improve readability

--- projectje3/projectje3/www/app.js
+++ projectje3/projectje3/www/app.js
@@ -1,85 +1,61 @@
+/*
+	Ik doe het even hier sinds dit oo kdeels om algemene shit gaat.
+	
+	Verwijder alle .js files enzo die je niet gebruikt, want nu zit je soms iets te lezen zonder dat het nuttig is
+	Haal console.log's weg
+	Haalf whitespaces e.d. weg
+	Haal mid function comments weg
+	
+	Daarnaast zoals Rizki mij ook al op wees, maak eventueel gebruik van een namespace
+		var APP = APP || {};
+
+*/
+
 (function () {
 	'use strict';
     
-
-    var sensors = {
-        compass: null,
-        geo: null
-    
-    
-    
-    }
-    
-    
+	
+    	var sensors = {
+        	compass: null,
+        	geo: null
+    	}
 
 	var controller = {
 		init: function () {
-           
-            compass.startWatch();
-            //foursquare.search();
-            geo.startWatch();
-            geo.startgame(locaties);
-            navigator.splashscreen.hide();
-            },
-        
-    }
+            		compass.startWatch();
+            		geo.startWatch();
+            		geo.startgame(locaties);
+            		navigator.splashscreen.hide();
+            	}
+    	}
     
-    var locaties = {
-        locatie1: {
-            naam: "De Dam",
-            lat: 52.373094,
-            long: 4.892535
-        },
-    
-         locatie2: {
-    
-        naam: "Rijksmuseum",
-        lat: 52.3599347,
-        long: 4.8849932
-     
-        
-    },
-    
-        locatie3: {
-            
-        naam: "Artis",
-        latitude: 52.366265,
-        longitude: 4.9166406
-        
-    },
-        
-        
-        
-    locatie4: {
-        
-        naam: "Dat leuke hotel",
-        latitude: 52.345864,
-        longitude: 4.918034
-        
-    },
-        
-    locatie5: {
-    
-        naam: "Tropenmuseum",
-        latitude: 52.3576569,
-        longitude: 4.9259487
-        
-    },
-        
-    Locatie6: {
-        
-        naam: "Station Amsterdam Zuid",
-        latitude: 52.3421159,
-        longitude: 4.869446
-	},
-    
-    
-   
-        
-        
-    }
+    	var locaties = {
+        	locatie1: {
+            		naam: "De Dam",
+            		lat: 52.373094,
+            		long: 4.892535
+        	}, locatie2: {
+        		naam: "Rijksmuseum",
+        		lat: 52.3599347,
+        		long: 4.8849932
+        	}, locatie3: {
+        		naam: "Artis",
+        		latitude: 52.366265,
+			longitude: 4.9166406
+    		}, locatie4: {
+        		naam: "Dat leuke hotel",
+        		latitude: 52.345864,
+        		longitude: 4.918034
+    		}, locatie5: {
+        		naam: "Tropenmuseum",
+        		latitude: 52.3576569,
+        		longitude: 4.9259487
+        	}, Locatie6: {
+        		naam: "Station Amsterdam Zuid",
+        		latitude: 52.3421159,
+        		longitude: 4.869446
+		}
+	 }
 
 	document.addEventListener("deviceready", controller.init, false);
-    
-   
 })();
